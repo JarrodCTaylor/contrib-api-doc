5bbb79c5d2be49d639e17974a113d0359de77f8b
JarrodCTaylor <jarrod@fastmail.com>
2023-08-08 14:29:49 -0500

    BETA4

--- build.sh
+++ build.sh
@@ -2,8 +2,6 @@ PROJECT="$(echo "$1" | rev | cut -d/ -f1 | rev)"
 
 cd contrib-api-doc
 
-cp -r build.ci/* .
-
 version="$(cd repo && mvn -q -N org.codehaus.mojo:exec-maven-plugin:1.3.1:exec -Dexec.executable='echo' -Dexec.args='${project.version}' | tail -1)"
 echo "version=$version"
 
